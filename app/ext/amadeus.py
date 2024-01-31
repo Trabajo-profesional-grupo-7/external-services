2bf30346c15b4f8cc098899396fff7fa4c3e3389
julietamponti <jponti@fi.uba.ar>
2024-01-31 18:48:04 -0300

    Add flight information service
    Co-authored-by: Matías Rotondo <MatiasRotondo@users.noreply.github.com>

---
+++ app/ext/amadeus.py
@@ -0,0 +1,31 @@
+import os
+
+from amadeus import Client, ResponseError
+from fastapi import status
+
+from app.services.flight_services import parse_flight_info
+from app.utils.api_exception import APIException
+from app.utils.constants import *
+
+API_KEY = os.getenv("AMADEUS_API_KEY")
+API_SECRET = os.getenv("AMADEUS_API_SECRET")
+
+amadeus = Client(client_id=API_KEY, client_secret=API_SECRET)
+
+
+def get_flight_info(carrier_code: str, flight_number: str, departure_date: str):
+    try:
+        response = amadeus.schedule.flights.get(
+            carrierCode=carrier_code,
+            flightNumber=flight_number,
+            scheduledDepartureDate=departure_date,
+        )
+
+        if not response.data:
+            raise APIException(
+                code=FLIGTH_INFO_NOT_FOUND, msg="FLIGHT INFORMATION NOT FOUND"
+            )
+
+        return parse_flight_info(response.data[0])
+    except ResponseError as e:
+        raise APIException(code=FLIGTH_INFO_NOT_FOUND, msg=str(e))
---
+++ app/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
+
+from app.routers.currency import router as currency_router
+from app.routers.flights import router as flights_router
+from app.routers.weather import router as weather_router
+
+app = FastAPI(title="External Services")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    max_age=3600,
+)
+
+app.include_router(flights_router)
+
+
+@app.get("/", include_in_schema=False)
+async def docs_redirect():
+    return RedirectResponse(url="/docs")
